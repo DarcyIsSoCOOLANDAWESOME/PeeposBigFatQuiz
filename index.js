f2a6bc48082a56177dbf672c69be907f91e555a0
‘darcy’ <darcy.mears@yahoo.com>
2026-04-28 13:54:28 +0100

    First commit test

---
+++ index.js
@@ -0,0 +1,28 @@
+//Basic Idea//
+//Peepo's Big Quiz! (my very epic character)
+//Peepo asks a question
+//You answer!
+//Answer from 4 choices. Only 1 is correct
+//Incorrect answer or Correct answer
+
+//Some Question examples!//
+//In the manga One Piece, who's dream is to be king of the pirates?
+//Luffy
+//Who holds the current deadlift world record?
+//Hafthor Bjornson (CORRECT)
+//Who won Worlds Strongest Man in 2025?
+//Rayno Nel
+//What was the original name for Spongebob?
+//Sponge Boy (CORRECT)
+//Sponge Man (INCORRECT)
+
+//EXTRA if i have time:
+//Can pick questions from categories
+
+// Button if button is pressed > display no button and start the game??
+//If (button pressed == true) {start()}
+
+//Need a function or a loop for the questions?
+//if correctAnswer === true ++point (If the answer is true, add a point)
+//if correctAnswer !== true ===point (If the correct answer is not true, keep points the same)
+//Choice from 4 answers. (3 answers) !==true (1 answer) === true (3 answers are not true, 1 answer is true)
